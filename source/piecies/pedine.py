680f0df34213e5851315fe2cd473619f6be9bc10
RelaxOrIFrsb <fraricci9@gmail.com>
2025-04-19 19:26:16 +0200

    Creata la v2 del file py pedine (non ancora completato il codice)

--- source/piecies/pedine.py
+++ source/piecies/pedine.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 import re;
 
+#nella mia idea la classe scachhiera andra a controllar lei se il pedone esiste o meno nella mossa del giocatore e poi
+#passera la mossa alla pedina per controllare se è valida o meno
+
+#pensandoci bene il pedone non deve sapere se è presente o meno sulla scacchiera e nemmeno la sua posizione iniziale
+#la scacchiera deve sapere se il pedone è presente o meno e la sua posizione iniziale
+#la pedina deve sapere solo se la mossa è valida o meno
 
 patternSingolaMossa = re.compile(r"^[a-h][1-8]$")
 patternMossaCompleta = re.compile(r"^[a-h][1-8] [a-h][1-8]$")
 coloriAccettati = ["bianco", "nero"]
 
 class Pedina(ABC):
-
-    
-
-
+ 
 
     def __init__(self, nome,colore, posizioneIniziale):
         if not isinstance(nome, str) or not isinstance(colore, str) or not isinstance(posizioneIniziale, str):
@@ -23,11 +26,11 @@ class Pedina(ABC):
             self.nome = nome
             self.colore = colore.lower()
             self.posizioneIniziale = posizioneIniziale
-            self.posizioneAttuale = posizioneIniziale
+            
         
     
     def __str__(self):
-        return f"{self.nome} {self.colore} {self.posizioneAttuale}"
+        return f"{self.nome} {self.colore}"
     #getters
     def getNome(self):
         return self.nome
@@ -35,8 +38,6 @@ class Pedina(ABC):
         return self.colore
     def getPosizioneIniziale(self):
         return self.posizioneIniziale
-    def getPosizioneAttuale(self):
-        return self.posizioneAttuale
     #setters
     def setNome(self, nome):
         self.nome = nome
@@ -44,13 +45,64 @@ class Pedina(ABC):
         self.colore = colore
     def setPosizioneIniziale(self, posizioneIniziale):
         self.posizioneIniziale = posizioneIniziale
-    def setPosizioneAttuale(self, posizioneAttuale):    
-        self.posizioneAttuale = posizioneAttuale
     #metodi astratti
     @abstractmethod
-    def mossePossibili(self):
+    def convalidaMossa(self):
         pass
 
 
 
-pedoni=Pedina("pedone", "bianco", "a2")
+class Pedone(Pedina):
+    def __init__(self,colore, posizioneIniziale):
+        super().__init__("Pedone", colore, posizioneIniziale)
+      
+    def __str__(self):
+        return super().__str__()
+    
+    def convalidaMossa(self,mossa):
+        if not isinstance(mossa, str):
+            raise TypeError("La mossa deve essere una stringa")
+        elif patternMossaCompleta.match(mossa) is None:
+            raise ValueError("La mossa inserita non è valida. Inserire la mossa in notazione algebraica (es a2 a4)")
+        elif self.colore == "bianco":
+            posizioneAttuale = mossa.split(" ")[0]
+            posiziioneFinale = mossa.split(" ")[1]
+            
+            if posizioneAttuale ==self.posizioneIniziale:  #siamo nel caso iniziale del pedone dobbiamo poter dare la possibilità di muoverci di 1 o 2
+                if posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])-1:
+                    return True
+                elif posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])-2:
+                    return True
+                else:   
+                    return False
+            else: #siamo nel caso in cui il pedone non è alla sua posizione iniziale
+                if posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])-1:
+                    return True
+                else:   
+                    return False #non può muoversi di 2 o comunque non puo andare indietro neanche a sinistra o destra
+        elif self.colore == "nero":
+            posizioneAttuale =mossa.split(" ")[0]
+            posiziioneFinale = mossa.split(" ")[1]
+            if posizioneAttuale ==self.posizioneIniziale:
+                if posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])+1:
+                    return True
+                elif posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])+2:
+                    return True
+                else:   
+                    return False
+            else: 
+                if posizioneAttuale[0]== posiziioneFinale[0] and int(posizioneAttuale[1])==int(posiziioneFinale[1])+1:
+                    return True
+                else:   
+                    return False
+
+        
+
+
+
+pedone=Pedone("bianco", "a2")
+print(pedone)
+if pedone.convalidaMossa("a2 a4"):
+    print("Mossa valida")
+else:
+    print("Mossa non valida")
